5c4fce5efe85e62fa8c76b9cc875e72447c2920d
ROHIN PRAMOD <rohinpramod320@gmail.com>
2024-12-01 21:08:17 +0530

    bcrypt

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const { createOrder, getAllOrders, getOrderById, getAllRestaurantOrders, updateOrderUser, updateOrderStatus } = require('../controllers/orderController');
 const { createPayment, verifyPayment } = require('../controllers/paymentController');
-
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.post('/create-order',createOrder);
-router.get('/get-all-order',getAllOrders);
+router.post('/create-order',roleMiddleware(['user']),createOrder);
+router.get('/get-all-order',roleMiddleware(['user']),getAllOrders);
 router.get('/get-order-by-id/:orderId',getOrderById);
-router.put('/update-Order/:orderId',updateOrderUser);
+router.put('/update-Order/:orderId',roleMiddleware(['user']),updateOrderUser);
 router.patch('/update-order-status/:orderId',updateOrderStatus);
 router.get('/get-all-restaurant-orders/:restaurantId',getAllRestaurantOrders);
 
-router.post('/:orderId/payment',createPayment);
-router.post('/verify-payment',verifyPayment);
+router.post('/:orderId/payment',roleMiddleware(['user']),createPayment);
+router.post('/verify-payment',roleMiddleware(['user']),verifyPayment);
 
 module.exports = router;
