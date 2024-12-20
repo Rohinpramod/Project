10301e49b8a935e63299870443fcf761e3e34b5f
ROHIN PRAMOD <rohinpramod320@gmail.com>
2024-12-21 00:48:20 +0530

    button comment

--- frontend/src/pages/User/Profile/Profile.jsx
+++ frontend/src/pages/User/Profile/Profile.jsx
@@ -91,11 +91,11 @@ const ProfilePage = () => {
             </li>
           </ul>
           <div className="mt-4">
-            <Link to="/contact">
+            {/* <Link to="/contact">
               <button className="btn bg-orange-400 hover:bg-orange-500 text-white px-4 py-2 rounded-md">
                 Edit Settings
               </button>
-            </Link>
+            </Link> */}
           </div>
         </div>
         <SavedAddresses addresses={profile.data.addresses} />
