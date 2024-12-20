168e8d088db254d164be3a880969c8b9e3373003
ROHIN PRAMOD <rohinpramod320@gmail.com>
2024-12-20 19:16:08 +0530

    limit 50mb

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser()); 
 app.use(cors({
     origin:[process.env.CLIENT_URL,process.env.ADMIN_URL],
